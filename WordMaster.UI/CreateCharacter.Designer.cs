7f0df90025f5835668853714eac513c520acda46
BHumblot <benoit.humblot@orange.fr>
2014-11-21 12:22:47 +0100

    Implement CreateCharacter Form.

---
+++ WordMaster.UI/CreateCharacter.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WordMaster.UI
+{
+    partial class CreateCharacter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AvatarLbl = new System.Windows.Forms.Label();
+            this.NameLbl = new System.Windows.Forms.Label();
+            this.DescriptionLbl = new System.Windows.Forms.Label();
+            this.BrowseLbl = new System.Windows.Forms.Button();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PathLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AvatarPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AvatarLbl
+            // 
+            this.AvatarLbl.AutoSize = true;
+            this.AvatarLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AvatarLbl.Location = new System.Drawing.Point(153, 152);
+            this.AvatarLbl.Name = "AvatarLbl";
+            this.AvatarLbl.Size = new System.Drawing.Size(152, 29);
+            this.AvatarLbl.TabIndex = 0;
+            this.AvatarLbl.Text = "Your avatar : ";
+            // 
+            // NameLbl
+            // 
+            this.NameLbl.AutoSize = true;
+            this.NameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLbl.Location = new System.Drawing.Point(157, 203);
+            this.NameLbl.Name = "NameLbl";
+            this.NameLbl.Size = new System.Drawing.Size(148, 29);
+            this.NameLbl.TabIndex = 1;
+            this.NameLbl.Text = "Your name : ";
+            // 
+            // DescriptionLbl
+            // 
+            this.DescriptionLbl.AutoSize = true;
+            this.DescriptionLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DescriptionLbl.Location = new System.Drawing.Point(98, 245);
+            this.DescriptionLbl.Name = "DescriptionLbl";
+            this.DescriptionLbl.Size = new System.Drawing.Size(207, 29);
+            this.DescriptionLbl.TabIndex = 2;
+            this.DescriptionLbl.Text = "Your description : ";
+            this.DescriptionLbl.Click += new System.EventHandler(this.DescriptionLbl_Click);
+            // 
+            // BrowseLbl
+            // 
+            this.BrowseLbl.Location = new System.Drawing.Point(593, 152);
+            this.BrowseLbl.Name = "BrowseLbl";
+            this.BrowseLbl.Size = new System.Drawing.Size(90, 26);
+            this.BrowseLbl.TabIndex = 3;
+            this.BrowseLbl.Text = "Browse";
+            this.BrowseLbl.UseVisualStyleBackColor = true;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameTextBox.Location = new System.Drawing.Point(320, 206);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(363, 26);
+            this.NameTextBox.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(320, 245);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(468, 205);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(799, 378);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // PathLbl
+            // 
+            this.PathLbl.AutoSize = true;
+            this.PathLbl.Location = new System.Drawing.Point(326, 161);
+            this.PathLbl.Name = "PathLbl";
+            this.PathLbl.Size = new System.Drawing.Size(29, 13);
+            this.PathLbl.TabIndex = 7;
+            this.PathLbl.Text = "Path";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(286, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 42);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Word Master";
+            // 
+            // AvatarPictureBox
+            // 
+            this.AvatarPictureBox.Location = new System.Drawing.Point(689, 112);
+            this.AvatarPictureBox.Name = "AvatarPictureBox";
+            this.AvatarPictureBox.Size = new System.Drawing.Size(98, 119);
+            this.AvatarPictureBox.TabIndex = 10;
+            this.AvatarPictureBox.TabStop = false;
+            // 
+            // CreateCharacter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 462);
+            this.Controls.Add(this.AvatarPictureBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PathLbl);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.BrowseLbl);
+            this.Controls.Add(this.DescriptionLbl);
+            this.Controls.Add(this.NameLbl);
+            this.Controls.Add(this.AvatarLbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "CreateCharacter";
+            this.Text = "CreateCharacter";
+            this.Load += new System.EventHandler(this.CreateCharacter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AvatarLbl;
+        private System.Windows.Forms.Label NameLbl;
+        private System.Windows.Forms.Label DescriptionLbl;
+        private System.Windows.Forms.Button BrowseLbl;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label PathLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox AvatarPictureBox;
+    }
+}
---
+++ WordMaster.UI/CreateCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordMaster.UI
+{
+    public partial class CreateCharacter : Form
+    {
+        public CreateCharacter()
+        {
+            InitializeComponent( );
+        }
+
+        private void CreateCharacter_Load( object sender, EventArgs e )
+        {
+
+        }
+
+        private void DescriptionLbl_Click( object sender, EventArgs e )
+        {
+
+        }
+
+        private void pictureBox1_Click( object sender, EventArgs e )
+        {
+
+        }
+    }
+}

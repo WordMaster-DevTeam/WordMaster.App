4428ec9bd672115593e6bd50b1bf905373bf3bac
BHumblot <benoit.humblot@orange.fr>
2014-11-25 10:43:38 +0100

    MainMenu update
    - Create a CharacterResume to display in the Table layout in MainMenu.
    -Implement initialization of table layout, not working.

---
+++ WordMaster.UI/CharacterResume.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WordMaster.DLL;
+
+namespace WordMaster.UI
+{
+    public partial class CharacterResume : UserControl
+    {
+        public string name { get; set; }
+
+        public int level { get; set; }
+
+        public int health { get; set; }
+
+        public string dungeon { get; set; }
+
+        public CharacterResume()
+        {
+            InitializeComponent( );
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            NameLbl.Text += name;
+            LevelLbl.Text += level;
+            HealthLbl.Text += health;
+            DungeonLbl.Text += dungeon;
+
+        }
+    }
+}
